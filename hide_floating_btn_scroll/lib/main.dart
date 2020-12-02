5eea07530491e9c312cd338d384d98fe7c5e76dd
praharsh bhatt <prahu.bhatt@gmail.com>
2020-12-02 08:39:08 +0530

    my first commit

---
+++ hide_floating_btn_scroll/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:hide_floating_btn_scroll/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
